7629047f052112c08966733bec7921bca6c012ad
Pavel Kotlyar <pavel.kotlyar@gmail.com>
2017-04-01 12:19:12 +1100

    add seabed without collition

--- main.gd
+++ main.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var screen_size
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	print("Hello From GoDot")
-	pass
+	screen_size = get_viewport_rect().size
+	draw_seabed()
+
+#  this is the initial version and most-likely wil be refactored once I'll get a better understanding how it all works
+func draw_seabed():
+	# first of all we need to calculate how many grass blocks we need to add to the screen	
+	# we have 1 inital grass block on the Seabed.tscn scene
+	var initial_grass_block = load("res://Seabed.tscn").instance()
+	add_child(initial_grass_block)
+	var initial_grass_block_size = initial_grass_block.get_texture().get_size()
+	var number_of_grass_blocks_to_add = (screen_size.x / initial_grass_block_size.x) - 1 # since again, we already have one
+	
+	# now add all additional grass blocks to the screen
+	var last_added_grass_x_coordinate = initial_grass_block.get_pos().x
+	
+	for i in range(number_of_grass_blocks_to_add):
+		last_added_grass_x_coordinate += initial_grass_block_size.x
+		var new_grass_block = load("res://Seabed.tscn").instance()
+		new_grass_block.set_pos(Vector2(last_added_grass_x_coordinate, new_grass_block.get_pos().y))
+		add_child(new_grass_block)
