6634bb3a2094bb291af4b886999de0ff6d3c06f9
Pavel Kotlyar <pavel.kotlyar@gmail.com>
2017-04-01 16:17:23 +1100

    show speed var in the Inspector

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const SPEED = 200
+export var speed = 200
 
 var screen_size
 var player_size
@@ -18,13 +18,13 @@ func _fixed_process(delta):
 	var player_position = get_pos()
 	
 	if (player_position.y > half_of_player_size_y and Input.is_action_pressed("ui_up")):
-		move(Vector2(0, -(SPEED * delta)))
+		move(Vector2(0, -(speed * delta)))
 	
 	if (player_position.y < (screen_size.y - half_of_player_size_y) and Input.is_action_pressed("ui_down")):
-		move(Vector2(0, SPEED * delta))
+		move(Vector2(0, speed * delta))
 		
 	if (player_position.x > half_of_player_size_x and Input.is_action_pressed("ui_left")):
-		move(Vector2(-(SPEED * delta), 0))
+		move(Vector2(-(speed * delta), 0))
 		
 	if (player_position.x < (screen_size.x - half_of_player_size_x) and Input.is_action_pressed("ui_right")):
-		move(Vector2(SPEED * delta, 0))
+		move(Vector2(speed * delta, 0))
