ca4c76140c7bde11391fe58045254733bbd8d4f6
Pavel Kotlyar <pavel.kotlyar@gmail.com>
2017-04-01 16:06:19 +1100

    add sky + player collision

--- main.gd
+++ main.gd
@@ -4,13 +4,14 @@ var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	draw_seabed()
+	draw_scene("res://seabed.tscn")
+	draw_scene("res://sky.tscn")
 
 #  this is the initial version and most-likely wil be refactored once I'll get a better understanding how it all works
-func draw_seabed():
+func draw_scene(scene_path):
 	# first of all we need to calculate how many grass blocks we need to add to the screen	
 	# we have 1 inital grass block on the Seabed.tscn scene
-	var initial_grass_block = load("res://Seabed.tscn").instance()
+	var initial_grass_block = load(scene_path).instance()
 	add_child(initial_grass_block)
 	var initial_grass_block_size = initial_grass_block.get_node("Sprite").get_texture().get_size()
 	var number_of_grass_blocks_to_add = (screen_size.x / initial_grass_block_size.x) - 1 # since again, we already have one
@@ -20,6 +21,6 @@ func draw_seabed():
 	
 	for i in range(number_of_grass_blocks_to_add):
 		last_added_grass_x_coordinate += initial_grass_block_size.x
-		var new_grass_block = load("res://Seabed.tscn").instance()
+		var new_grass_block = load(scene_path).instance()
 		new_grass_block.set_pos(Vector2(last_added_grass_x_coordinate, new_grass_block.get_pos().y))
 		add_child(new_grass_block)
