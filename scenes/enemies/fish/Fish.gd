96ebb65a8523449f29ee200956b59a0d8bbb3f49
Pavel Kotlyar <pavel.kotlyar@gmail.com>
2017-04-07 21:22:05 +1000

    WIP fish behaviour

--- scenes/enemies/fish/Fish.gd
+++ scenes/enemies/fish/Fish.gd
@@ -1,11 +1,23 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+export var speed = 400
+
+onready var screen_size = get_viewport_rect().size
+var motion = Vector2()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	print("Hello From Fish")
-	pass
+	set_initial_position()
+	set_fixed_process(true)
+
+func set_initial_position():
+	randomize()
+	var inital_y_position = int(rand_range(128, screen_size.y - 128))
+	set_pos(Vector2(0, inital_y_position))
+
+func _fixed_process(delta):
+	motion += Vector2(1, 0)
+	motion = motion.normalized() * speed * delta
+	move(motion)
+	
+	if(get_pos().x > screen_size.x + get_node("Sprite").get_texture().get_size().x):
+		set_initial_position()
