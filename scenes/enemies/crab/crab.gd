7090bc7729e600730560a33a82a3ae29895f7083
Pavel Kotlyar <pavel.kotlyar@gmail.com>
2017-04-09 16:25:23 +1000

    show crab after random time interval

--- scenes/enemies/crab/crab.gd
+++ scenes/enemies/crab/crab.gd
@@ -2,12 +2,14 @@ extends KinematicBody2D
 
 onready var sprite_size = get_node("Sprite").get_texture().get_size()
 onready var screen_size = get_viewport_rect().size
+onready var timer = get_node("Timer")
 
 var speed
 var moving_from_left_to_right = false
+var timeout = true
 
 func _ready():
-	set_initial_position()
+	set_initial_position_after_timeout()
 	set_fixed_process(true)
 
 func set_initial_position():
@@ -25,16 +27,31 @@ func set_initial_position():
 		moving_from_left_to_right = false
 		set_pos(start_from_right)
 
+func set_initial_position_after_timeout():
+	timeout = true
+	set_initial_position()
+	timer.connect("timeout", self, "its_show_time")
+	randomize()
+	timer.set_wait_time(int(rand_range(1, 5)))
+	timer.start()
+	
+func its_show_time():
+	timeout = false
+
 func _fixed_process(delta):
+	if !timeout:
+	  move_crab(delta)
+	
+func move_crab(delta):
 	var motion = Vector2()
 	if moving_from_left_to_right:
 		motion += Vector2(1, 0)
 		if get_pos().x > screen_size.x + sprite_size.x:
-			set_initial_position()
+			set_initial_position_after_timeout()
 	else:
 		motion += Vector2(-1, 0)
 		if get_pos().x < -sprite_size.x:
-			set_initial_position()
+			set_initial_position_after_timeout()
 		
 	motion = motion.normalized() * speed * delta
 	move(motion)
